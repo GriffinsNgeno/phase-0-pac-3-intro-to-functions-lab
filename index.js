d130581ee824ad7da20842f10483bc54b3aeceab
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2022-11-21 10:02:56 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+const string ='Hello'
+
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase()){
+        var deaf = 'I can\'t hear you!';
+        return deaf
+    } else if (string === string.toUpperCase()){
+        var deaf = 'YES INDEED!';
+        return deaf
+    } else {
+        var feels = 'I would love to!'; 
+        return feels
+    }
+
+}
